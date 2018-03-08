f0e41521b39ef63ca71d102c99911f1a104c2d9e
Dillon <34174536+TingeOGinge@users.noreply.github.com>
2018-03-08 10:46:49 +0000

    Create Digits-to-Ints.cpp

---
+++ Digits-to-Ints.cpp
@@ -0,0 +1,24 @@
+/*
+Input - Conversion (char vector to int) - Print 
+
+User inputs a stream of chars, each of which is placed into a vector. 
+- Maximum 4 digits to start 
+
+Function needed to convert char vector into a single int 
+- i < vector.size(); 
+-   10 * x + vector[i]  //This appears to be the correct formula 
+
+Another vector is needed with corresponding unit measurements 
+- vector<string>v { "Thousand(s)", "Hundred(s)", "Ten(s)", "One(s)" };
+
+print function will then output the int value, followed by the breakdown of chars and string measurements 
+-	cout << value << " is "; 
+-	x < charv.size();
+-		cout << charv[x] << " " << stringv[i + 4 - charv.size();]
+
+*/
+
+int main()
+{
+	
+}
